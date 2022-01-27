b438873902a922e9fbfb413aa95522a753eef9d7
David Van Goidtsnoven <vangoidtsnovendavid@gmail.com>
2022-01-27 12:05:28 +0100

    push

---
+++ assets/css/style.tsx
@@ -0,0 +1,19 @@
+const styles : object  = {
+        container: {
+            flex: 1,
+            backgroundColor: '#f5e7da',
+            alignItems: 'center',
+            justifyContent: 'space-around'
+        },
+        input: {
+            width: 200,
+            height: 40,
+            borderWidth: 1,
+            padding: 10
+        },
+        label: {
+            textAlign: "center"
+        }
+}
+
+export default styles;
